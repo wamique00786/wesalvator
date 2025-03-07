6ee53981208328f62b81c79098d7a5c0bfbbd6c0
Aakash Kumar <aakashkumar113450@gmail.com>
2025-03-07 13:38:41 +0530

    fixing missing import for LoginView

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -18,6 +18,9 @@ import logging
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
+
 
 logger = logging.getLogger(__name__)
 
